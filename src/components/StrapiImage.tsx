e3fad1a49e97af7f1a98d3a9d622ce9672a36fc7
Laila Ben <161597125+Laila-front-dev@users.noreply.github.com>
2025-06-17 16:47:31 +0100

    Update StrapiImage.tsx

--- src/components/StrapiImage.tsx
+++ src/components/StrapiImage.tsx
@@ -18,6 +18,6 @@ export default StrapiImage;
 export function getStrapiMedia(url: string | null) {
   if (url == null) return null;
   if (url.startsWith("data:")) return url;
-  if (url.startsWith("https") || url.startsWith("//")) return url;
+  if (url.startsWith("http") || url.startsWith("//")) return url;
   return getStapiUrl() + url;
 }
